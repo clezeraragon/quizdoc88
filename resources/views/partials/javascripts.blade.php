727f8e5625ba84ac54cc87f1ee471c8c1af72b71
Clezer <clezer.aragon@doc88.com.br>
2018-11-27 14:07:37 -0200

    https

--- resources/views/partials/javascripts.blade.php
+++ resources/views/partials/javascripts.blade.php
@@ -10,7 +10,7 @@
     window._token = '{{ csrf_token() }}';
 </script>
 
-<script src="{{ asset('js/app.js') }}"></script>
+<script src="{{ asset('js/app.js',true) }}"></script>
 
 
 @yield('javascript')
